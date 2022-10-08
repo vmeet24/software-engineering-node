7ee192e788a11cfc258fa3ce65daa8ec487d5baf
vmeet24 <meetvora1998@gmail.com>
2022-10-08 14:45:58 -0400

    connected to mongodb

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 /**
  * @file Implements an Express Node HTTP server.
  */
-import express, {Request, Response} from 'express';
-const cors = require('cors')
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import UserController from './Controllers/UserController';
+import UserDao from './mongoose/UserDao';
+const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/tuitDB');
+
+const userDao = new UserDao();
+new UserController(app, userDao);
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Welcome to Foundation of Software Engineering!!!!'));
@@ -18,4 +25,6 @@ app.get('/hello', (req: Request, res: Response) =>
  * but use environment variable PORT on Heroku if available.
  */
 const PORT = 4000;
-app.listen(process.env.PORT || PORT);
+app.listen(process.env.PORT || PORT, () => {
+    console.log("Up and Running!");
+});
