908d2c65b6faea9876316795cb65071cabc418a9
vmeet24 <meetvora1998@gmail.com>
2022-10-08 00:41:23 -0400

    added user mongoose dao class

---
+++ mongoose/UserDao.ts
@@ -0,0 +1,22 @@
+import IUserDao from "../interfaces/IUserDao";
+import User from "../models/User";
+import UserModel from "./UserModel";
+
+export default class UserDao implements IUserDao {
+
+    async findAllUsers(): Promise<User[]> {
+        return UserModel.find();
+    }
+    async findUserById(uid: string): Promise<any> {
+        return UserModel.findById(uid);
+    }
+    async createUser(user: User): Promise<User> {
+        return UserModel.create(user);
+    }
+    async updateUser(uid: string, user: User): Promise<any> {
+        return UserModel.updateOne({ _id: uid }, { $set: user });
+    }
+    async deleteUser(uid: string): Promise<any> {
+        return UserModel.deleteOne({ _id: uid });
+    }
+}
