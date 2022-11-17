1936fb12aa29f81c33e8eba16530a6a43fc2a428
vmeet24 <meetvora1998@gmail.com>
2022-11-16 19:38:09 -0500

    updated user model

--- models/User.ts
+++ models/User.ts
@@ -6,19 +6,19 @@ import AccountType from "./AccountType";
 import MaritalStatus from "./MaritalStatus";
 import Location from "./Location";
 
-export default class User {
-   private username: string = '';
-   private password: string = '';
-   private firstName: string | null = null;
-   private lastName: string | null = null;
-   private email: string = '';
-   private profilePhoto: string | null = null;
-   private headerImage: string | null = null;
-   private accountType: AccountType = AccountType.Personal;
-   private maritalStatus: MaritalStatus = MaritalStatus.Single;
-   private biography: string | null = null;
-   private dateOfBirth: Date | null = null;
-   private joined: Date = new Date();
-   private location: Location | null = null;
+export default interface User {
+   username: string;
+   password: string;
+   firstName: string | null;
+   lastName: string | null;
+   email: string;
+   profilePhoto: string | null;
+   headerImage: string | null;
+   accountType: AccountType;
+   maritalStatus: MaritalStatus;
+   biography: string | null;
+   dateOfBirth: Date | null;
+   joined: Date;
+   location: Location | null;
 }
 
